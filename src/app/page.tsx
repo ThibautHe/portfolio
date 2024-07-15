41dde19b3b64870743020c68e3c615350bb09f4e
Tea-bow <thibaut.hellinckx@hotmail.com>
2024-07-15 20:31:14 +0200

    first step

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,170 @@
+"use client";
+import { motion } from "framer-motion";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  const ref = useRef<HTMLDivElement>(null);
+  const [dimensions, setDimensions] = useState<{ w: number; h: number } | null>(
+    null
+  );
+
+  let dots = [];
+
+  const w = ref.current?.offsetWidth;
+  const h = ref.current?.offsetHeight;
+  console.log(w!);
+
+  for (let index = 0; index < 36; index++) {
+    dots.push(
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: index * 0.05, duration: 1 }}
+        key={index}
+        className="bg-white block w-[2px] h-[2px] rounded-full"
+      ></motion.span>
+    );
+  }
+
+  useEffect(() => {
+    if (ref.current) {
+      const w = ref.current.offsetWidth;
+      const h = ref.current.offsetHeight;
+      setDimensions({ w, h });
+    }
+  }, [ref.current]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <div className="flex justify-between p-6 items-center relative">
+        <nav>
+          <ul className="flex gap-4">
+            <li>Home</li>
+            <li>Portfolio</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+        <Hamburgermenu></Hamburgermenu>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div
+        ref={ref}
+        className="absolute z-50 left-[65%] top-[55%] -translate-x-1/2 -translate-y-1/2 w-[50vw] h-[90vh]"
+      >
+        {/* <Image style={{opacity:0}} src={"/bgpic.png"} width={800} height={800}></Image> */}
+        <div
+          style={{
+            height: `${h! / 4 - 5}px`,
+            top: `${(h! * 0) / 4}px`,
+            left: "-100px",
+            backgroundSize: w! + 400 + "px",
+            backgroundPosition: `calc(50% + -50px) -${(0 * h!) / 4 + 190}px`,
+            backgroundRepeat: "no-repeat",
+          }}
+          className={`absolute w-full left-0 bg-mainimg z-10`}
+        ></div>
+        <div
+          style={{
+            height: `${h! / 4 - 5}px`,
+            top: `${(h! * 1) / 4}px`,
+            left: "-50px",
+            backgroundSize: w! + 400 + "px",
+            backgroundPosition: `calc(50% - 100px) -${(1 * h!) / 4 + 190}px`,
+            backgroundRepeat: "no-repeat",
+          }}
+          className="absolute w-full left-0 bg-mainimg z-10 "
+        ></div>
+        <div
+          style={{
+            height: `${h! / 4 - 5}px`,
+            top: `${(h! * 2) / 4}px`,
+            left: "50px",
+            backgroundSize: w! + 400 + "px",
+            backgroundPosition: `calc(50% - 200px) -${(2 * h!) / 4 + 190}px`,
+            backgroundRepeat: "no-repeat",
+          }}
+          className="absolute w-full left-0 bg-mainimg z-10 "
+        ></div>
+        <div
+          style={{
+            height: `${h! / 4 - 5}px`,
+            top: `${(h! * 3) / 4}px`,
+            left: "0px",
+            backgroundSize: w! + 400 + "px",
+            backgroundPosition: `calc(50% - 150px) -${(3 * h!) / 4 + 190}px`,
+            backgroundRepeat: "no-repeat",
+          }}
+          className="absolute w-full left-0 bg-mainimg z-10 "
+        ></div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="grid w-[600px] -translate-y-1/2 -translate-x-1/2 left-1/2 grid-cols-12 gap-12 absolute">
+        {dots}
       </div>
-    </main>
+      <div className="grid -translate-y-1/2 -translate-x-1/2 top-1/2 right-0 grid-cols-3 gap-12 absolute">
+        {dots}
+      </div>
+      <Lines></Lines>
+    </>
   );
 }
+
+const Lines = () => {
+  return (
+    <div className="flex flex-col gap-12 absolute -left-8 w-fit top-1/2 ">
+      <motion.span
+        key={"1"}
+        initial={{ width: "0", transform: "Rotate(0)" }}
+        animate={{ width: "24rem", transform: "Rotate(-30deg)" }}
+        transition={{ delay: 0 }}
+        className="bg-white block w-96 h-px -rotate-[30deg]"
+      ></motion.span>
+      <motion.span
+        key={"2"}
+        initial={{ width: "0", transform: "Rotate(0)" }}
+        animate={{ width: "24rem", transform: "Rotate(-30deg)" }}
+        transition={{ delay: 0.2 }}
+        className="bg-white block w-96 h-px -rotate-[30deg]"
+      ></motion.span>
+      <motion.span
+        key={"3"}
+        initial={{ width: "0", transform: "Rotate(0)" }}
+        animate={{ width: "24rem", transform: "Rotate(-30deg)" }}
+        transition={{ delay: 0.4 }}
+        className="bg-white block w-96 h-px -rotate-[30deg]"
+      ></motion.span>
+      <motion.span
+        key={"4"}
+        initial={{ width: "0", transform: "Rotate(0)" }}
+        animate={{ width: "24rem", transform: "Rotate(-30deg)" }}
+        transition={{ delay: 0.6 }}
+        className="bg-white block w-96 h-px -rotate-[30deg]"
+      ></motion.span>
+      <motion.span
+        key={"5"}
+        initial={{ width: "0", transform: "Rotate(0)" }}
+        animate={{ width: "24rem", transform: "Rotate(-30deg)" }}
+        transition={{ delay: 0.8 }}
+        className="bg-white block w-96 h-px -rotate-[30deg]"
+      ></motion.span>
+      <motion.span
+        key={"6"}
+        initial={{ width: "0", transform: "Rotate(0)" }}
+        animate={{ width: "24rem", transform: "Rotate(-30deg)" }}
+        transition={{ delay: 1, duration: 0.5 }}
+        className="bg-white block w-96 h-px -rotate-[30deg]"
+      ></motion.span>
+    </div>
+  );
+};
+
+const Hamburgermenu = () => {
+  return (
+    <div className=" w-fit flex flex-col gap-2 justify-center ">
+      <span className="bg-white block w-6 h-1 rounded-full"></span>
+      <span className="bg-white block w-10 h-1 rounded-full"></span>
+      <span className="bg-white block w-10 h-1 rounded-full"></span>
+      <span className="bg-white block w-6 h-1 rounded-full self-end"></span>
+    </div>
+  );
+};
